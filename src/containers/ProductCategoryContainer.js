f9a67b1465f89d941cd50243222a6f12542bc950
Huanhuan Huang <hhuang@atlassian.com>
2017-07-09 18:05:51 +1000

    Change solution port to 3001.

--- src/containers/ProductCategoryContainer.js
+++ src/containers/ProductCategoryContainer.js
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
 import ProductCategory from '../components/ProductCategory'
 import { setCategory } from '../actionsAndReducers/ticket'
 
@@ -11,4 +10,4 @@ const mapStateToProps = (state) => ({
   ticket: state.ticket,
 })
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ProductCategory))
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCategory)
